dd23050334fcf6509fed431aaa6918155cf54133
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2020-02-25 13:16:38 -0500

    Configure alias for server/

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   webpack(config) {
-    const aliasedDirectories = ["components", "utils"];
+    const aliasedDirectories = ["components", "utils", "server"];
     aliasedDirectories.forEach(dir => {
       config.resolve.alias[dir] = path.join(__dirname, dir);
     });
