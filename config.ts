23a52dd17b0280ffa4121d74b62a9d886212aea4
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2020-04-14 00:52:54 -0500

    Config updates

--- config.ts
+++ config.ts
@@ -1,12 +1,13 @@
 export default {
+  // Use these env variables client side may not work
   dbUrl: process.env.DB_URL,
   dbName: process.env.DB_NAME,
   jwtSecret: process.env.JWT_SECRET,
   stripeSecret: process.env.STRIPE_SECRET,
-  stripePublishable: process.env.STRIPE_PUBLISHABLE,
   pages: {
     index: "/",
-    login: "/login"
+    login: "/login",
+    donate: (path = "[id]") => `/donate/${path}`
   },
   apis: {
     login: "/api/login",
@@ -14,6 +15,7 @@ export default {
     checkToken: "/api/checkToken",
     createDonation: "/api/createDonation",
     createNonprofit: "/api/createNonprofit",
+    paymentIntents: "/api/paymentIntents",
     // Remove this endpoint when we no longer need to redirect from index.ts
     getDefaultNonprofitId: "/api/getDefaultNonprofitId"
   }
