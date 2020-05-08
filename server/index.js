75e35ed1016b08c2a50bcedd9888d66e9060e671
Max Karpawich <max.karpawich@gmail.com>
2020-05-08 18:27:29 -0400

    remove secrets, replace with production envvars

--- server/index.js
+++ server/index.js
@@ -4,11 +4,6 @@ import config from "config";
 export default async () => {
   if (mongoose.connections[0].readyState) return;
 
-  console.log("db_url" + config.dbUrl);
-  console.log("db_name" + config.dbName);
-  console.log("jwt_secret" + config.jwtSecret);
-  console.log("stripe_secret" + config.stripeSecret);
-
   await mongoose
     .connect(config.dbUrl, {
       useNewUrlParser: true,
