14798afe8eab4917d7f029da6fceaecec93ee9c3
Fattimo <matt.chen522@gmail.com>
2020-09-10 06:40:47 -0400

    adjusted comments, made dates not required, changed volunteer/nonprofit id type, added default empty string for about

--- server/models/event.js
+++ server/models/event.js
@@ -1,30 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// TODO: Volunteer schema
-// Keep in sync with utils/types Donation
 const eventSchema = new Schema({
   name: {
     type: String,
     required: true
   },
   nonprofitId: {
-    type: String,
+    type: Schema.ObjectId,
     ref: "Nonprofit",
     required: true
   },
   startDate: {
     type: Date,
-    required: true,
     default: Date.now
   },
   endDate: {
     type: Date,
-    required: true,
     default: Date.now
   },
   about: {
-    type: String
+    type: String,
+    default: ""
   },
   maxVolunteers: {
     type: Number,
@@ -32,8 +29,8 @@ const eventSchema = new Schema({
   },
   volunteers: [
     {
-      type: mongoose.Schema.ObjectId,
-      ref: "Volunteer" //TODO
+      type: Schema.ObjectId,
+      ref: "Admin" //TODO replace with "User" once that schema is created
     }
   ],
   image: {
