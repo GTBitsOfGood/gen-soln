0e14e71c3447e92db41ec52bfc69396388061926
Matthew Brown <matthewbrown@gatech.edu>
2020-09-10 09:58:13 -0400

    Creates root component for event page (#122)

---
+++ components/events/EventsPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import EventsPageLayout from "components/events/EventsPageLayout";
+
+const EventsPage: React.FC = () => {
+  return <EventsPageLayout sidebarComponent={null}>{null}</EventsPageLayout>;
+};
+
+export default EventsPage;
