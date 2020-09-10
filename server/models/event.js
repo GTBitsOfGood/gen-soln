d00d0535a0fb0668b0ba72b09c7128462719751e
Jade Law <jadelaw71700@hotmail.com>
2020-09-10 13:40:17 -0400

    added point schema to event.js

--- server/models/event.js
+++ server/models/event.js
@@ -1,7 +1,56 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  nonprofitId: {
+    type: Schema.ObjectId,
+    ref: "Nonprofit",
+    required: true
+  },
+  startDate: {
+    type: Date,
+    default: Date.now
+  },
+  endDate: {
+    type: Date,
+    default: Date.now
+  },
+  about: {
+    type: String,
+    default: ""
+  },
+  maxVolunteers: {
+    type: Number,
+    required: true
+  },
+  volunteers: {
+    type: [
+      {
+        type: Schema.ObjectId,
+        ref: "Admin" //TODO replace with "User" once that schema is created
+      }
+    ],
+    default: []
+  },
+  image: {
+    type: String,
+    default: 'url("/defaultImages/defaultEvent.png")'
+  }
+});
+
+/**
+ * @return the duration of the event in ms
+ */
+eventSchema.virtual("duration").get(function () {
+  return this.endDate - this.startDate;
+});
 
 // eslint-disable-next-line no-unused-vars
-const pointSchema = new mongoose.Schema({
+const pointSchema = new Schema({
   type: {
     type: String,
     enum: ["Point"],
@@ -12,3 +61,5 @@ const pointSchema = new mongoose.Schema({
     required: true
   }
 });
+
+module.exports = mongoose.models?.Event || mongoose.model("Event", eventSchema);
