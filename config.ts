ddbdee24565b8e9dc8f9971332c5029be50e1cca
Michael Chen <michael.ld.chen@gmail.com>
2020-09-13 17:03:48 -0400

    Rename apis.paymentIntents to apis.createPaymentIntent

--- config.ts
+++ config.ts
@@ -30,7 +30,7 @@ export default {
     login: "/api/login",
     checkToken: "/api/checkToken",
     logDonation: "/api/logDonation",
-    paymentIntents: "/api/createPaymentIntent",
+    createPaymentIntent: "/api/createPaymentIntent",
     // Remove this endpoint when we no longer need to redirect from index.ts
     getDefaultNonprofitId: "/api/getDefaultNonprofitId"
   }
