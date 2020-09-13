7037bb7f6d866689ddedd82475babe4431ee6dd7
Matt Chen <matt.chen522@gmail.com>
2020-09-13 16:02:43 -0400

    created getNearestEvents function in events.ts. fixed typo in event.js

--- server/actions/events.ts
+++ server/actions/events.ts
@@ -16,3 +16,28 @@ export async function getUpcomingEvents(): Promise<EventType[]> {
 
   return result;
 }
+
+export async function getNearestEvents(
+  lat: number,
+  long: number
+): Promise<EventType[]> {
+  await Mongo();
+
+  const result = (await Event.find(
+    {
+      "address.location": {
+        $nearSphere: {
+          $geometry: {
+            type: "Point",
+            coordinates: [lat, long]
+          }
+        }
+      }
+    },
+    {}
+  )
+    .lean()
+    .limit(5)) as EventType[];
+
+  return result;
+}
