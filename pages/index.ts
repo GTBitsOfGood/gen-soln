f6f3ba751e971363e532ab22d8356c14b74b14d0
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2020-09-14 15:04:47 -0400

    improvement

--- pages/index.ts
+++ pages/index.ts
@@ -1,25 +1,27 @@
 import { useEffect } from "react";
-import { NextPage } from "next";
+import { NextPage, GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import { useRouter } from "next/router";
 import urls from "config";
 
-import { getDefaultNonprofitIdRequest } from "requests/nonprofit";
+import { getDefaultNonprofitId } from "server/actions/nonprofit";
 
-const IndexPage: NextPage = () => {
+const IndexPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  defaultNonprofitId
+}) => {
   /* TODO: For now, we don't show anything and redirect users to a default non-profit's donation page.
    * Modify this behavior when the actual IndexPage UI gets built. */
   const router = useRouter();
 
   useEffect(() => {
-    const fn = async () => {
-      const id = await getDefaultNonprofitIdRequest();
-      void router.push(urls.pages.donate(id));
-    };
-
-    void fn();
-  }, [router]);
+    void router.push(urls.pages.donate(defaultNonprofitId));
+  }, [router, defaultNonprofitId]);
 
   return null;
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const getStaticProps = async () => {
+  return { props: { defaultNonprofitId: await getDefaultNonprofitId() } };
 };
 
 export default IndexPage;
