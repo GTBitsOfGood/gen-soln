f8ab6cd219db8227cb16a7fb6cac31404088e7ed
Miguel de los Reyes <thisiswhataninjalookslike@gmail.com>
2020-09-14 11:02:30 -0400

    Add events page left rail component

---
+++ components/events/EventsPageLeftRailComponent.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    padding: "32px 24px",
+    width: "100%"
+  },
+  header: {
+    // TODO: replace this with a Typography component
+    fontFamily: "Visby CF, sans-serif",
+    fontWeight: 800,
+    fontSize: 24,
+    lineHeight: "140%",
+    color: "#333333"
+  },
+  subtitle: {
+    // TODO: replace this with a Typography component
+    fontFamily: "Visby CF, sans-serif",
+    fontWeight: 800,
+    fontSize: 16,
+    lineHeight: "130%",
+    color: "#333333"
+  },
+  subComponent: {
+    marginTop: 16
+  }
+});
+
+const EventsPageLeftRailComponent: React.FC<Record<string, unknown>> = () => {
+  const { root, header, subtitle, subComponent } = useStyles();
+  return (
+    <div className={root}>
+      <Typography className={header}>Filters</Typography>
+      <div className={subComponent}>
+        <Typography className={subtitle}>Location</Typography>
+      </div>
+    </div>
+  );
+};
+
+export default EventsPageLeftRailComponent;
