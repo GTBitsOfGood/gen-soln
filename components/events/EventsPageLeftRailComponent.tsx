0a163339b1865ad7f71e461619c9b1c03e548555
Jade Law <55326683+jade-law@users.noreply.github.com>
2020-09-20 21:15:14 -0400

    auto-complete location textfield (#162)
    * auto-complete location textfield
    
    * removed package-lock
    
    * styling and loadScript fix

--- components/events/EventsPageLeftRailComponent.tsx
+++ components/events/EventsPageLeftRailComponent.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Typography from "@material-ui/core/Typography";
 
+import EventsPageLocationFilterAutocompleteInput from "./EventsPageLocationFilterAutocompleteInput";
+
 const useStyles = makeStyles({
   root: {
     padding: "32px 24px",
@@ -27,16 +29,22 @@ const useStyles = makeStyles({
   },
   subComponent: {
     marginTop: 16
+  },
+  searchContainer: {
+    marginTop: 16
   }
 });
 
 const EventsPageLeftRailComponent: React.FC<Record<string, unknown>> = () => {
-  const { root, header, subtitle, subComponent } = useStyles();
+  const { root, header, subtitle, subComponent, searchContainer } = useStyles();
   return (
     <div className={root}>
       <Typography className={header}>Filters</Typography>
       <div className={subComponent}>
         <Typography className={subtitle}>Location</Typography>
+        <div className={searchContainer}>
+          <EventsPageLocationFilterAutocompleteInput />
+        </div>
       </div>
     </div>
   );
--- config.ts
+++ config.ts
@@ -34,5 +34,6 @@ export default {
     logDonation: "/api/logDonation",
     createPaymentIntent: "/api/createPaymentIntent",
     recoverPassword: "/api/recoverPassword"
-  }
+  },
+  googleMapsKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY
 };
