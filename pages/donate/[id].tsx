a6c9d9d2405d625eb464098d994a7c8a289155e1
Michael Chen <michael.ld.chen@gmail.com>
2020-09-22 19:23:18 -0400

    Attempt to regenerate nonprofit page

--- pages/donate/[id].tsx
+++ pages/donate/[id].tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { NextPage, GetStaticProps, GetStaticPaths } from "next";
+import {
+  NextPage,
+  GetStaticProps,
+  GetStaticPaths,
+  InferGetStaticPropsType,
+  GetStaticPropsContext
+} from "next";
 
 import { Dropdown } from "utils/types";
 
@@ -12,41 +18,68 @@ import {
 import DonationPage from "components/donation/DonationPage";
 
 import config from "config";
+import Head from "next/head";
+import DefaultErrorPage from "next/error";
 
-const NonprofitDonationPage: NextPage<React.ComponentProps<
-  typeof DonationPage
->> = props => <DonationPage {...props} />;
+const NonprofitDonationPage: NextPage<InferGetStaticPropsType<
+  typeof getStaticProps
+>> = props => {
+  if (props.nonprofit) {
+    // @ts-ignore temp
+    return <DonationPage {...props} />;
+  } else {
+    return (
+      <>
+        <Head>
+          <meta name="robots" content="noindex" />
+          <title>404</title>
+        </Head>
+        <DefaultErrorPage statusCode={404} />
+      </>
+    );
+  }
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const ids = await getNonprofitIds();
 
-  return { paths: ids.map(config.pages.donate), fallback: false };
+  return { paths: ids.map(config.pages.donate), fallback: true };
 };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const id = params?.id as string;
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  const id = context.params?.id as string;
 
   /* For now, getNonprofitById excludes Nonprofit's donation field. If you were fetching it, ensure
    * that donation IDs are mapped to a string array so that the nonprofit object is JSON serializable. */
-  const [namesWithIds, nonprofit] = await Promise.all([
-    getNonprofitNamesWithIds(),
-    getNonprofitById(id)
-  ]);
+  try {
+    const [namesWithIds, nonprofit] = await Promise.all([
+      getNonprofitNamesWithIds(),
+      getNonprofitById(id)
+    ]);
 
-  const items = namesWithIds.map(
-    ({ _id, name }): Dropdown => ({
-      value: _id,
-      text: name
-    })
-  );
+    const items = namesWithIds.map(
+      ({ _id, name }): Dropdown => ({
+        value: _id,
+        text: name
+      })
+    );
 
-  return {
-    props: {
-      nonprofit,
-      items,
-      selectedValue: id
-    }
-  };
+    return {
+      props: {
+        nonprofit,
+        items,
+        selectedValue: id
+      },
+      revalidate: 10
+    };
+  } catch (err) {
+    return {
+      props: {}
+    };
+  }
 };
 
 export default NonprofitDonationPage;
