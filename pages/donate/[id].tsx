e3122afa6a7b82da68b49db273b43f9626d9f092
Michael Chen <michaelchen@gatech.edu>
2020-09-22 22:24:58 -0400

    Regenerate donate page if the nonprofit id doesn't exist  (#184)
    * Attempt to regenerate nonprofit page
    
    * Resolve type error with getStaticProps
    
    * Remove unused imports, add revalidate for 404
    
    * Return null for fallback page

--- pages/donate/[id].tsx
+++ pages/donate/[id].tsx
@@ -1,52 +1,73 @@
 import React from "react";
-import { NextPage, GetStaticProps, GetStaticPaths } from "next";
-
+import {
+  NextPage,
+  GetStaticPaths,
+  InferGetStaticPropsType,
+  GetStaticPropsContext
+} from "next";
+import ErrorPage from "next/error";
 import { Dropdown } from "utils/types";
-
 import {
   getNonprofitIds,
   getNonprofitNamesWithIds,
   getNonprofitById
 } from "server/actions/nonprofit";
-
 import DonationPage from "components/donation/DonationPage";
-
 import config from "config";
+import { useRouter } from "next/router";
 
-const NonprofitDonationPage: NextPage<React.ComponentProps<
-  typeof DonationPage
->> = props => <DonationPage {...props} />;
+const NonprofitDonationPage: NextPage<InferGetStaticPropsType<
+  typeof getStaticProps
+>> = props => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return null;
+  } else if (props.nonprofit) {
+    return <DonationPage {...props} />;
+  } else {
+    return <ErrorPage statusCode={404} />;
+  }
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const ids = await getNonprofitIds();
 
-  return { paths: ids.map(config.pages.donate), fallback: false };
+  return { paths: ids.map(config.pages.donate), fallback: true };
 };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const id = params?.id as string;
+// eslint-disable-next-line
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const id = context.params?.id as string;
 
   /* For now, getNonprofitById excludes Nonprofit's donation field. If you were fetching it, ensure
    * that donation IDs are mapped to a string array so that the nonprofit object is JSON serializable. */
-  const [namesWithIds, nonprofit] = await Promise.all([
-    getNonprofitNamesWithIds(),
-    getNonprofitById(id)
-  ]);
+  try {
+    const [namesWithIds, nonprofit] = await Promise.all([
+      getNonprofitNamesWithIds(),
+      getNonprofitById(id)
+    ]);
 
-  const items = namesWithIds.map(
-    ({ _id, name }): Dropdown => ({
-      value: _id,
-      text: name
-    })
-  );
+    const items = namesWithIds.map(
+      ({ _id, name }): Dropdown => ({
+        value: _id,
+        text: name
+      })
+    );
 
-  return {
-    props: {
-      nonprofit,
-      items,
-      selectedValue: id
-    }
-  };
+    return {
+      props: {
+        nonprofit,
+        items,
+        selectedValue: id
+      },
+      revalidate: 10
+    };
+  } catch (err) {
+    return {
+      props: {},
+      revalidate: 10
+    };
+  }
 };
 
 export default NonprofitDonationPage;
