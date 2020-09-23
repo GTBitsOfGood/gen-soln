ef85f32ebc44e850176349cf7a3272e028e14272
Michael Chen <michael.ld.chen@gmail.com>
2020-09-22 20:44:28 -0400

    Resolve type error with getStaticProps

--- pages/donate/[id].tsx
+++ pages/donate/[id].tsx
@@ -19,24 +19,15 @@ import DonationPage from "components/donation/DonationPage";
 
 import config from "config";
 import Head from "next/head";
-import DefaultErrorPage from "next/error";
+import ErrorPage from "next/error";
 
 const NonprofitDonationPage: NextPage<InferGetStaticPropsType<
   typeof getStaticProps
 >> = props => {
   if (props.nonprofit) {
-    // @ts-ignore temp
     return <DonationPage {...props} />;
   } else {
-    return (
-      <>
-        <Head>
-          <meta name="robots" content="noindex" />
-          <title>404</title>
-        </Head>
-        <DefaultErrorPage statusCode={404} />
-      </>
-    );
+    return <ErrorPage statusCode={404} />;
   }
 };
 
@@ -46,10 +37,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   return { paths: ids.map(config.pages.donate), fallback: true };
 };
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const getStaticProps: GetStaticProps = async (
-  context: GetStaticPropsContext
-) => {
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params?.id as string;
 
   /* For now, getNonprofitById excludes Nonprofit's donation field. If you were fetching it, ensure
