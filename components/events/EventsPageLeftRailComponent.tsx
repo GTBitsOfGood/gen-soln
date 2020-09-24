0c72e495b554f09e0f545cd7381ea07e83bcecf8
mbclr3 <mbclr3@gatech.edu>
2020-09-24 10:17:55 -0400

    Add filter clearing functionality and UI

--- components/events/EventsPageLeftRailComponent.tsx
+++ components/events/EventsPageLeftRailComponent.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Typography from "@material-ui/core/Typography";
@@ -6,6 +7,7 @@ import EventsPageLeftRailFilter from "./EventsPageLeftRailFilter";
 import EventsPageTimeFilter from "./EventsPageTimeFilter";
 
 import EventsPageLocationFilterAutocompleteInput from "./EventsPageLocationFilterAutocompleteInput";
+import FocusVisibleOnly from "components/FocusVisibleOnly";
 
 const useStyles = makeStyles({
   root: {
@@ -20,14 +22,57 @@ const useStyles = makeStyles({
     fontSize: 24,
     lineHeight: "140%",
     color: "#333333"
+  },
+  topBar: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  clearFilterLabel: {
+    fontFamily: "Visby CF, sans-serif",
+    fontSize: 14,
+    lineHeight: "130%",
+    fontWeight: 800,
+    color: "#FD8033",
+    cursor: "pointer",
+    textTransform: "uppercase"
   }
 });
 
 const EventsPageLeftRailComponent: React.FC<Record<string, unknown>> = () => {
-  const { root, header } = useStyles();
+  const { root, header, topBar, clearFilterLabel } = useStyles();
+  const router = useRouter();
+
+  // Sum up all the applied filters
+  const filterCount = Object.values(router.query).reduce((acc, x) => {
+    // x is string | string[] | undefined, so we need to account for each case
+    if (x == null) {
+      return acc;
+    } else if (Array.isArray(x)) {
+      return acc + x.length;
+    }
+    return acc + 1;
+  }, 0);
+
+  const clearFilters = async () => {
+    await router.push({
+      query: {}
+    });
+  };
+
   return (
     <div className={root}>
-      <Typography className={header}>Filters</Typography>
+      <div className={topBar}>
+        <Typography className={header}>Filters</Typography>
+        {filterCount > 0 && (
+          <FocusVisibleOnly onClick={() => clearFilters()}>
+            <Typography className={clearFilterLabel}>
+              Clear ({filterCount})
+            </Typography>
+          </FocusVisibleOnly>
+        )}
+      </div>
       <EventsPageLeftRailFilter
         header="Location"
         content={<EventsPageLocationFilterAutocompleteInput />}
