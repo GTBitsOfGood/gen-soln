17e38b2093cf36f8129e14be9e4612c5f5659d15
Steven Chen <schen2425@gmail.com>
2020-09-24 14:22:40 -0400

    added causes to nonprofit schema

--- server/models/nonprofit.js
+++ server/models/nonprofit.js
@@ -5,6 +5,18 @@ const Schema = mongoose.Schema;
 const ALPHABET = "0123456789";
 const ID_LENGTH = 8;
 const nanoid = customAlphabet(ALPHABET, ID_LENGTH);
+const CAUSES = [
+  { text: "arts", value: "ARTS" },
+  { text: "Culture and Humanities", value: "CULTURE AND HUMANITIES" },
+  { text: "Education and Research", value: "EDUCATION AND RESEARCH" },
+  { text: "Environment and Animals", value: "ENVIRMONMENT AND ANIMALS" },
+  { text: "Health", value: "HEALTH" },
+  { text: "Human Services", value: "HUMAN SERVICES" },
+  { text: "International", value: "INTERNATIONAL" },
+  { text: "Public, Societal", value: "PUBLIC, SOCIETAL" },
+  { text: "Religion", value: "RELIGION" },
+  { text: "Other", value: "OTHER" }
+];
 
 // Keep in sync with utils/types Nonprofit
 const nonprofitSchema = new Schema({
@@ -62,8 +74,25 @@ const nonprofitSchema = new Schema({
       }
     ],
     default: []
+  },
+  cause: {
+    type: String,
+    enum: [
+      "ARTS, CULTURE, AND HUMANITIES",
+      "EDUCATION AND RESEARCH",
+      "ENVIRONMENT AND ANIMALS",
+      "HEALTH",
+      "HUMAN SERVICES",
+      "INTERNATIONAL",
+      "PUBLIC, SOCIETAL",
+      "RELIGION",
+      "OTHER"
+    ],
+    default: "OTHER",
+    required: true
   }
 });
 
 module.exports =
   mongoose.models?.Nonprofit || mongoose.model("Nonprofit", nonprofitSchema);
+module.exports.CAUSES = CAUSES;
