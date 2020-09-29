2f6881cc8447f45a252c416eb5e013d6f1953759
Cullen.Bertram <55326661+Cullen101@users.noreply.github.com>
2020-09-29 00:18:20 -0400

    Issue #104 Route Stripe Payments (#199)
    * test: adds stripeAccount changes
    
    * Updated comments on donation.ts
    
    Co-authored-by: Kris Satya <krissatya09@gmail.com>

--- server/actions/donation.ts
+++ server/actions/donation.ts
@@ -42,19 +42,24 @@ export async function logDonation({
  *
  * @param {number} amount - Amount (in US cents) to be collected by the PaymentIntent
  * @param {string} email - Email address to send the transaction receipt to
+ * @param {string} stripeAccount - stripe account we are sending payment to
  * @returns {Promise<string>} - client_secret of the PaymentIntent created
  */
 export async function createPaymentIntent({
   amount,
-  email
+  email,
+  stripeAccount
 }: NextApiRequest["body"]): Promise<Stripe.PaymentIntent["client_secret"]> {
   const stripe = stripeConstructor();
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount,
-    receipt_email: email,
-    currency: "usd"
-  });
+  const paymentIntent = await stripe.paymentIntents.create(
+    {
+      amount,
+      receipt_email: email,
+      currency: "usd"
+    },
+    { stripeAccount: stripeAccount }
+  );
 
   return paymentIntent.client_secret;
 }
