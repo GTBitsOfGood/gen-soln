fbec1af8fe9ce91e97dad4eea1298f5b1051aa85
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2020-09-29 14:30:15 -0400

    fix broken builds

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,24 +1,17 @@
 import React from "react";
-import { NextPage, GetServerSideProps, InferGetStaticPropsType } from "next";
+import { NextPage, InferGetServerSidePropsType } from "next";
 import EventsPage from "components/events/EventsPage";
 
 import { getCauses } from "server/actions/nonprofit";
 
-const EventsNextPage: NextPage<InferGetStaticPropsType<
-  typeof getStaticProps
+const EventsNextPage: NextPage<InferGetServerSidePropsType<
+  typeof getServerSideProps
 >> = props => {
   return <EventsPage causesFilterOptions={props.causesFilterOptions} />;
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  // make request here!
-  return {
-    props: {}
-  };
-};
-
 // eslint-disable-next-line @typescript-eslint/require-await
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   // Don't remove the async otherwise InferGetStaticPropsType won't work as expected
   return {
     props: { causesFilterOptions: getCauses() }
