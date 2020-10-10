72a27e840e7e15e264e8517bf7748c791b729e82
Matt Chen <matt.chen522@gmail.com>
2020-10-10 12:43:05 -0400

    return array of coordinates given array of address string (#261)
    * return array of coordinates given array of address string
    
    * removed Coordinates import, unsure if I should try to use typescript to regulate return type of method to be Coordinate[]
    
    * deleted package-lock.json. added filter to only US. changed return type to match multipolygon
    
    * package-lock delete

--- server/actions/events.ts
+++ server/actions/events.ts
@@ -2,7 +2,6 @@ import Mongo from "server/index";
 import Event from "server/models/event";
 import Nonprofit from "server/models/nonprofit";
 import errors from "utils/errors";
-
 import {
   Event as EventType,
   EventCardData as EventCardDataType,
@@ -11,6 +10,8 @@ import {
   LocationPaginatedEventCards,
   LocationPageInformation
 } from "utils/types";
+import { Client } from "@googlemaps/google-maps-services-js";
+import config from "config";
 
 const CARD_FIELDS: Record<keyof EventCardDataType, 1> = {
   name: 1,
@@ -150,4 +151,37 @@ export async function getAllEventIds(): Promise<string[]> {
   await Mongo();
 
   return Event.distinct("_id").exec();
+}
+
+export function getCityPolygonCoordinates(cities: string[]) {
+  const client = new Client({});
+
+  return Promise.all(
+    cities.map(async city => {
+      const geocode = await client.geocode({
+        params: {
+          address: city, // space delineated street address of location
+          components: "country:US",
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          key: config.googleMapsKey!
+        }
+      });
+
+      const viewport = geocode.data.results[0].geometry.viewport;
+
+      const north = viewport.northeast.lng;
+      const east = viewport.northeast.lat;
+      const south = viewport.southwest.lng;
+      const west = viewport.southwest.lat;
+
+      return [
+        [
+          [north, east],
+          [south, east],
+          [south, west],
+          [north, west]
+        ]
+      ];
+    })
+  );
 }
