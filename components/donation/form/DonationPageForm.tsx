e0a797708dba1f09c1ca17996930ee5122193a16
Cullen Bertram <cullen.bertram@gmail.com>
2020-10-12 18:54:23 -0400

    Updated createPaymentMethod to take address fields from google autocomplete

--- components/donation/form/DonationPageForm.tsx
+++ components/donation/form/DonationPageForm.tsx
@@ -142,12 +142,24 @@ const DonationPageForm: React.FC<Props> = ({
           nonprofitId: selectedNonprofitId
         });
       } else {
-        isPaymentStep &&
-          (await createPaymentMethod(
+        if (isPaymentStep && billingStep.address) {
+          const [
+            city,
+            state,
+            country
+          ] = billingStep.address.structured_formatting.secondary_text.split(
+            ", "
+          );
+          await createPaymentMethod(
             name,
             billingStep.email,
-            billingStep.zipcode
-          ));
+            billingStep.zipcode,
+            city,
+            billingStep.address.structured_formatting.main_text,
+            state,
+            country.slice(0, -1)
+          );
+        }
         dispatch(incrementStep());
       }
     },
