15badd2efc8bcd816417463414006ca39537dabe
Matt Chen <matt.chen522@gmail.com>
2020-11-06 16:02:31 -0500

    added default img for cause types (#355)
    * added default img for cause types
    
    * added a TODO to replace default image in event model
    
    * added back defaultEvent.png, deleted this in addition to defaultCause.png by mistake
    
    * removed todo
    
    * moved imgs to causes folder, added background image properties to the csss to make sure that it covers the container

--- components/events/EventsPageCauseList.tsx
+++ components/events/EventsPageCauseList.tsx
@@ -12,7 +12,7 @@ const CAUSE_CARDS: PaginatedCauseCards = {
   cards: filters["cause"].map(({ text, value }) => {
     return {
       cause: text,
-      imagePath: "/defaultImages/defaultCause.png",
+      imagePath: `/causes/${value}.jpg`,
       filterValue: value
     };
   }),
