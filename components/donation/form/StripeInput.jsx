0abe13109c0e5e9c4231c9dcac6ebae66615b3d9
Michael Chen <michael.ld.chen@gmail.com>
2020-11-11 22:14:37 -0500

    Set font size and font family of Stripe input

--- components/donation/form/StripeInput.jsx
+++ components/donation/form/StripeInput.jsx
@@ -1,27 +1,10 @@
 import React, { useState, useImperativeHandle } from "react";
 
-import { fade, createStyles, makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles(({ palette, typography }) =>
-  createStyles({
-    base: {
-      color: palette.text.primary,
-      fontSize: typography.fontSize,
-      fontFamily: typography.fontFamily,
-      "::placeholder": {
-        color: fade(palette.text.primary, 0.42)
-      }
-    },
-    invalid: {
-      color: palette.text.primary
-    }
-  })
-);
+import { typographyStyles } from "@core/typography";
 
 // Code based on https://codesandbox.io/s/stripe-0xez4
 const StripeInput = props => {
   const { component: Component, inputRef, ...other } = props;
-  const { base, invalid } = useStyles();
   const [mountNode, setMountNode] = useState(null);
 
   useImperativeHandle(
@@ -32,13 +15,17 @@ const StripeInput = props => {
     [mountNode]
   );
 
-  // TODO: These styles don't seem to be doing anything?
   return (
     <Component
       onReady={setMountNode}
-      style={{
-        base,
-        invalid
+      options={{
+        style: {
+          base: {
+            fontSize: typographyStyles.body1.fontSize,
+            // For some reason the font family "OpenSans-Regular" does not seem to be loaded
+            fontFamily: `${typographyStyles.body1.fontFamily}, "Open Sans", "sans-serif"`
+          }
+        }
       }}
       {...other}
     />
